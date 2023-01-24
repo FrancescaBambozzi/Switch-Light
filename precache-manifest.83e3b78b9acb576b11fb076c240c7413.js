472e7df490f8ff1ddd5f54436c9ed4c7afda91ff
FrancescaBambozzi <bambozzif@gmail.com>
2023-01-24 15:37:28 +0100

    Updates

---
+++ precache-manifest.83e3b78b9acb576b11fb076c240c7413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6d9ef12359821e3a07a",
+    "url": "/Switch-Light/static/css/main.2156ec17.chunk.css"
+  },
+  {
+    "revision": "b6d9ef12359821e3a07a",
+    "url": "/Switch-Light/static/js/main.08f1d7bf.chunk.js"
+  },
+  {
+    "revision": "34fed66c4c36628c35ef",
+    "url": "/Switch-Light/static/js/runtime~main.9bfee6ce.js"
+  },
+  {
+    "revision": "6978b117402f10549e57",
+    "url": "/Switch-Light/static/js/2.daa065df.chunk.js"
+  },
+  {
+    "revision": "60fff737e85bee6665d495c4c8746123",
+    "url": "/Switch-Light/index.html"
+  }
+];
